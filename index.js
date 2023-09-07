2413ac05d04070825ea58f8d50e3ae5152cc2e0a
luis <messibarbro@gmail.com>
2023-09-06 21:34:17 -0700

    cleaner version

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Q_container = document.getElementById('question-container');
 const score = document.getElementById('score');
 let i = 0;
 let correctCount = 0;
+let inputPicked = null;
 btn.style.background = "rgb(0, 132, 255)"
 
 let questionList = [
@@ -54,14 +55,8 @@ function populateQuestions(){
     labelB.textContent = `B) ${questionList[i].b.text}`;
 }
 
-
-populateQuestions()
-
-
-
-function check(){
-    let inputPicked = null;
-
+function reset(){
+     
     if(!score.classList.contains("hide")){
         btn.textContent = "Next"
         score.classList.add('hide')
@@ -70,6 +65,9 @@ function check(){
         btn.style.background = "rgb(0, 132, 255)"
         return
     }
+}
+
+function checkInputChosen(){
 
     inputs.forEach(inp=>{
         if(inp.checked){
@@ -81,12 +79,10 @@ function check(){
    if(inputPicked == null){
        return
    }
+}
 
-   if(questionList[i][inputPicked].isCorrect){
-        correctCount++
-   }
-
-   if(i >= questionList.length-1){
+function isLastQuestion(){
+    if(i >= questionList.length-1){
         i = 0
         let total = questionList.length
         Q_container.classList.remove('question-container')
@@ -101,7 +97,30 @@ function check(){
     else{
         i++
     }
+}
+
+populateQuestions()
+
+
+
+function Next(){
+    inputPicked = null
+    // check if score element is hidden if not then hide it and reset everything
+    reset()
+
+    // check which choice the user made and if its null than do not go to the next question
+    checkInputChosen()
+
+//    check if choice is correct if yes increment correctCount
+
+   if(questionList[i][inputPicked].isCorrect){
+        correctCount++
+   }
+
+//    check for last question if so dont increment i and show score
+    isLastQuestion()
+    // update current Question
     populateQuestions()
 }
 
-btn.addEventListener('click',check);
+btn.addEventListener('click',Next);
